1809bea5fa39921ca2e95c16944203f42b5c880d
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-03-07 11:39:53 +0100

    add initial data preprocessing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,62 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
+import argparse
+import warnings
+import pandas as pd
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+warnings.filterwarnings("ignore")
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    root_dir = Path(__file__).parent.parent.parent
+    data_dir = root_dir / 'data'
+    input_filepath = data_dir / input_filepath
+    output_filepath = data_dir / output_filepath
+    # 1. Read ground truth data
+    ground_truth_file = input_filepath / 'movement_pain.csv'
+    ground_truth = pd.read_csv(ground_truth_file, sep=';')
+    for index, row in ground_truth.iterrows():
+        # 2. Read Skeleton data
+        for folder in input_filepath.iterdir():
+            if str(folder.name) == row['scan_id']:
+                df = pd.read_csv(folder / 'skeleton.csv')
+                df['pain_level'] = row['pain_area'] + \
+                    ' - Level ' + str(row['level'])
+                # 3. Save as processed data
+                i = 1
+                for processed_file in (output_filepath / 'skeleton').iterdir():
+                    if processed_file.name == str(folder.name) + '_1.csv':
+                        if i < 2:
+                            i = 2
+                    elif processed_file.name == str(folder.name) + '_2.csv':
+                        if i < 3:
+                            i = 3
+                    elif processed_file.name == str(folder.name) + '_3.csv':
+                        if i < 4:
+                            i = 4
+                    elif processed_file.name == str(folder.name) + '_4.csv':
+                        if i < 5:
+                            i = 5
+                    elif processed_file.name == str(folder.name) + '_5.csv':
+                        if i < 6:
+                            i = 6
+                    elif processed_file.name == str(folder.name) + '_6.csv':
+                        if i < 7:
+                            i = 7
+                    elif processed_file.name == str(folder.name) + '_7.csv':
+                        if i < 8:
+                            i = 8
+                    elif processed_file.name == str(folder.name) + '_8.csv':
+                        if i < 9:
+                            i = 9
+                file_name = str(folder.name) + '_' + str(i) + '.csv'
+                df.to_csv(
+                    output_filepath / 'skeleton' / file_name, index=False)
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_filepath', type=str, default='raw')
+    parser.add_argument('--output_filepath', type=str, default='processed')
+    args = parser.parse_args()
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(args.input_filepath, args.output_filepath)
